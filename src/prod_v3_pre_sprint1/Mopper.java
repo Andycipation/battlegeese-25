1aa21b4a528c820e4e855d1db7d128e414cb999d
Max Jiang <maxjiang123@gmail.com>
2025-01-15 17:17:56 -0500

    Add prod_v3

---
+++ src/prod_v3_pre_sprint1/Mopper.java
@@ -0,0 +1,16 @@
+package prod_v3_pre_sprint1;
+
+import battlecode.common.*;
+
+public class Mopper extends Robot {
+
+    Mopper() {
+        super();
+    }
+
+    @Override
+    void play() throws GameActionException {
+        
+    }
+    
+}
