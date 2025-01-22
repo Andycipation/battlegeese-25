97d1213b2488badc868f0d14fde8b6644dea6662
Max Jiang <maxjiang123@gmail.com>
2025-01-22 00:24:13 -0500

    Add dirInEnemyTowerRange

--- src/prod_latest/Robot.java
+++ src/prod_latest/Robot.java
@@ -14,6 +14,8 @@ public abstract class Robot extends Globals {
     static Message[] lastRoundMessages;
     // numAllyAdjacent is indexed by the same was as Direction.getDirectionOrderNum
     public static int[] numAllyAdjacent;
+    public static int inEnemyTowerRangeMask;
+
 
     public static final int CHIP_AGGREGATION_WINDOW = 50;
     public static int[] chipCountQueue;
@@ -38,6 +40,11 @@ public abstract class Robot extends Globals {
         // double std = Math.sqrt(EX2 - EX * EX);
         // System.out.println("" + EX + " " + std);
         // return EX / std;
+    }
+
+    // is my current location plus dir in an enemy tower range?
+    public boolean dirInEnemyTowerRange(Direction dir) {
+        return (1 & (inEnemyTowerRangeMask >> dir.getDirectionOrderNum())) == 1;
     }
 
     /**
@@ -84,7 +91,149 @@ public abstract class Robot extends Globals {
                 case 22: numAllyAdjacent[4]++; break;
             }
         }
-        // relatively cheap startup, takes around <1000 bytecodes up to here even when cluttered
+        for (int i = nearbyEnemyRobots.length; --i >= 0;) {
+            RobotInfo enemy = nearbyEnemyRobots[i];
+            if (!enemy.getType().isTowerType()) continue;
+            MapLocation diff = enemy.getLocation().translate(-locBeforeTurn.x, -locBeforeTurn.y);
+            if (isDefenseTower(enemy.getType())) {
+                switch ((diff.x + 4) * 9 + (diff.y + 4)) {
+                    case 2: inEnemyTowerRangeMask |= 258; break; // (-4, -2)
+                    case 3: inEnemyTowerRangeMask |= 390; break; // (-4, -1)
+                    case 4: inEnemyTowerRangeMask |= 263; break; // (-4, 0)
+                    case 5: inEnemyTowerRangeMask |= 270; break; // (-4, 1)
+                    case 6: inEnemyTowerRangeMask |= 6; break; // (-4, 2)
+                    case 10: inEnemyTowerRangeMask |= 386; break; // (-3, -3)
+                    case 11: inEnemyTowerRangeMask |= 391; break; // (-3, -2)
+                    case 12: inEnemyTowerRangeMask |= 463; break; // (-3, -1)
+                    case 13: inEnemyTowerRangeMask |= 431; break; // (-3, 0)
+                    case 14: inEnemyTowerRangeMask |= 415; break; // (-3, 1)
+                    case 15: inEnemyTowerRangeMask |= 271; break; // (-3, 2)
+                    case 16: inEnemyTowerRangeMask |= 14; break; // (-3, 3)
+                    case 18: inEnemyTowerRangeMask |= 384; break; // (-2, -4)
+                    case 19: inEnemyTowerRangeMask |= 451; break; // (-2, -3)
+                    case 20: inEnemyTowerRangeMask |= 495; break; // (-2, -2)
+                    case 21: inEnemyTowerRangeMask |= 511; break; // (-2, -1)
+                    case 22: inEnemyTowerRangeMask |= 511; break; // (-2, 0)
+                    case 23: inEnemyTowerRangeMask |= 511; break; // (-2, 1)
+                    case 24: inEnemyTowerRangeMask |= 447; break; // (-2, 2)
+                    case 25: inEnemyTowerRangeMask |= 31; break; // (-2, 3)
+                    case 26: inEnemyTowerRangeMask |= 12; break; // (-2, 4)
+                    case 27: inEnemyTowerRangeMask |= 450; break; // (-1, -4)
+                    case 28: inEnemyTowerRangeMask |= 487; break; // (-1, -3)
+                    case 29: inEnemyTowerRangeMask |= 511; break; // (-1, -2)
+                    case 30: inEnemyTowerRangeMask |= 511; break; // (-1, -1)
+                    case 31: inEnemyTowerRangeMask |= 511; break; // (-1, 0)
+                    case 32: inEnemyTowerRangeMask |= 511; break; // (-1, 1)
+                    case 33: inEnemyTowerRangeMask |= 511; break; // (-1, 2)
+                    case 34: inEnemyTowerRangeMask |= 319; break; // (-1, 3)
+                    case 35: inEnemyTowerRangeMask |= 30; break; // (-1, 4)
+                    case 36: inEnemyTowerRangeMask |= 449; break; // (0, -4)
+                    case 37: inEnemyTowerRangeMask |= 491; break; // (0, -3)
+                    case 38: inEnemyTowerRangeMask |= 511; break; // (0, -2)
+                    case 39: inEnemyTowerRangeMask |= 511; break; // (0, -1)
+                    case 40: inEnemyTowerRangeMask |= 511; break; // (0, 0)
+                    case 41: inEnemyTowerRangeMask |= 511; break; // (0, 1)
+                    case 42: inEnemyTowerRangeMask |= 511; break; // (0, 2)
+                    case 43: inEnemyTowerRangeMask |= 191; break; // (0, 3)
+                    case 44: inEnemyTowerRangeMask |= 29; break; // (0, 4)
+                    case 45: inEnemyTowerRangeMask |= 480; break; // (1, -4)
+                    case 46: inEnemyTowerRangeMask |= 499; break; // (1, -3)
+                    case 47: inEnemyTowerRangeMask |= 511; break; // (1, -2)
+                    case 48: inEnemyTowerRangeMask |= 511; break; // (1, -1)
+                    case 49: inEnemyTowerRangeMask |= 511; break; // (1, 0)
+                    case 50: inEnemyTowerRangeMask |= 511; break; // (1, 1)
+                    case 51: inEnemyTowerRangeMask |= 511; break; // (1, 2)
+                    case 52: inEnemyTowerRangeMask |= 127; break; // (1, 3)
+                    case 53: inEnemyTowerRangeMask |= 60; break; // (1, 4)
+                    case 54: inEnemyTowerRangeMask |= 192; break; // (2, -4)
+                    case 55: inEnemyTowerRangeMask |= 481; break; // (2, -3)
+                    case 56: inEnemyTowerRangeMask |= 507; break; // (2, -2)
+                    case 57: inEnemyTowerRangeMask |= 511; break; // (2, -1)
+                    case 58: inEnemyTowerRangeMask |= 511; break; // (2, 0)
+                    case 59: inEnemyTowerRangeMask |= 511; break; // (2, 1)
+                    case 60: inEnemyTowerRangeMask |= 255; break; // (2, 2)
+                    case 61: inEnemyTowerRangeMask |= 61; break; // (2, 3)
+                    case 62: inEnemyTowerRangeMask |= 24; break; // (2, 4)
+                    case 64: inEnemyTowerRangeMask |= 224; break; // (3, -3)
+                    case 65: inEnemyTowerRangeMask |= 241; break; // (3, -2)
+                    case 66: inEnemyTowerRangeMask |= 505; break; // (3, -1)
+                    case 67: inEnemyTowerRangeMask |= 251; break; // (3, 0)
+                    case 68: inEnemyTowerRangeMask |= 253; break; // (3, 1)
+                    case 69: inEnemyTowerRangeMask |= 121; break; // (3, 2)
+                    case 70: inEnemyTowerRangeMask |= 56; break; // (3, 3)
+                    case 74: inEnemyTowerRangeMask |= 96; break; // (4, -2)
+                    case 75: inEnemyTowerRangeMask |= 240; break; // (4, -1)
+                    case 76: inEnemyTowerRangeMask |= 113; break; // (4, 0)
+                    case 77: inEnemyTowerRangeMask |= 120; break; // (4, 1)
+                    case 78: inEnemyTowerRangeMask |= 48; break; // (4, 2)
+                }
+            }
+            else {
+                switch ((diff.x + 4) * 9 + (diff.y + 4)) {
+                    case 3: inEnemyTowerRangeMask |= 256; break; // (-4, -1)
+                    case 4: inEnemyTowerRangeMask |= 2; break; // (-4, 0)
+                    case 5: inEnemyTowerRangeMask |= 4; break; // (-4, 1)
+                    case 10: inEnemyTowerRangeMask |= 256; break; // (-3, -3)
+                    case 11: inEnemyTowerRangeMask |= 258; break; // (-3, -2)
+                    case 12: inEnemyTowerRangeMask |= 390; break; // (-3, -1)
+                    case 13: inEnemyTowerRangeMask |= 263; break; // (-3, 0)
+                    case 14: inEnemyTowerRangeMask |= 270; break; // (-3, 1)
+                    case 15: inEnemyTowerRangeMask |= 6; break; // (-3, 2)
+                    case 16: inEnemyTowerRangeMask |= 4; break; // (-3, 3)
+                    case 19: inEnemyTowerRangeMask |= 384; break; // (-2, -3)
+                    case 20: inEnemyTowerRangeMask |= 387; break; // (-2, -2)
+                    case 21: inEnemyTowerRangeMask |= 463; break; // (-2, -1)
+                    case 22: inEnemyTowerRangeMask |= 431; break; // (-2, 0)
+                    case 23: inEnemyTowerRangeMask |= 415; break; // (-2, 1)
+                    case 24: inEnemyTowerRangeMask |= 15; break; // (-2, 2)
+                    case 25: inEnemyTowerRangeMask |= 12; break; // (-2, 3)
+                    case 27: inEnemyTowerRangeMask |= 256; break; // (-1, -4)
+                    case 28: inEnemyTowerRangeMask |= 450; break; // (-1, -3)
+                    case 29: inEnemyTowerRangeMask |= 487; break; // (-1, -2)
+                    case 30: inEnemyTowerRangeMask |= 511; break; // (-1, -1)
+                    case 31: inEnemyTowerRangeMask |= 511; break; // (-1, 0)
+                    case 32: inEnemyTowerRangeMask |= 511; break; // (-1, 1)
+                    case 33: inEnemyTowerRangeMask |= 319; break; // (-1, 2)
+                    case 34: inEnemyTowerRangeMask |= 30; break; // (-1, 3)
+                    case 35: inEnemyTowerRangeMask |= 4; break; // (-1, 4)
+                    case 36: inEnemyTowerRangeMask |= 128; break; // (0, -4)
+                    case 37: inEnemyTowerRangeMask |= 449; break; // (0, -3)
+                    case 38: inEnemyTowerRangeMask |= 491; break; // (0, -2)
+                    case 39: inEnemyTowerRangeMask |= 511; break; // (0, -1)
+                    case 40: inEnemyTowerRangeMask |= 511; break; // (0, 0)
+                    case 41: inEnemyTowerRangeMask |= 511; break; // (0, 1)
+                    case 42: inEnemyTowerRangeMask |= 191; break; // (0, 2)
+                    case 43: inEnemyTowerRangeMask |= 29; break; // (0, 3)
+                    case 44: inEnemyTowerRangeMask |= 8; break; // (0, 4)
+                    case 45: inEnemyTowerRangeMask |= 64; break; // (1, -4)
+                    case 46: inEnemyTowerRangeMask |= 480; break; // (1, -3)
+                    case 47: inEnemyTowerRangeMask |= 499; break; // (1, -2)
+                    case 48: inEnemyTowerRangeMask |= 511; break; // (1, -1)
+                    case 49: inEnemyTowerRangeMask |= 511; break; // (1, 0)
+                    case 50: inEnemyTowerRangeMask |= 511; break; // (1, 1)
+                    case 51: inEnemyTowerRangeMask |= 127; break; // (1, 2)
+                    case 52: inEnemyTowerRangeMask |= 60; break; // (1, 3)
+                    case 53: inEnemyTowerRangeMask |= 16; break; // (1, 4)
+                    case 55: inEnemyTowerRangeMask |= 192; break; // (2, -3)
+                    case 56: inEnemyTowerRangeMask |= 225; break; // (2, -2)
+                    case 57: inEnemyTowerRangeMask |= 505; break; // (2, -1)
+                    case 58: inEnemyTowerRangeMask |= 251; break; // (2, 0)
+                    case 59: inEnemyTowerRangeMask |= 253; break; // (2, 1)
+                    case 60: inEnemyTowerRangeMask |= 57; break; // (2, 2)
+                    case 61: inEnemyTowerRangeMask |= 24; break; // (2, 3)
+                    case 64: inEnemyTowerRangeMask |= 64; break; // (3, -3)
+                    case 65: inEnemyTowerRangeMask |= 96; break; // (3, -2)
+                    case 66: inEnemyTowerRangeMask |= 240; break; // (3, -1)
+                    case 67: inEnemyTowerRangeMask |= 113; break; // (3, 0)
+                    case 68: inEnemyTowerRangeMask |= 120; break; // (3, 1)
+                    case 69: inEnemyTowerRangeMask |= 48; break; // (3, 2)
+                    case 70: inEnemyTowerRangeMask |= 16; break; // (3, 3)
+                    case 75: inEnemyTowerRangeMask |= 64; break; // (4, -1)
+                    case 76: inEnemyTowerRangeMask |= 32; break; // (4, 0)
+                    case 77: inEnemyTowerRangeMask |= 16; break; // (4, 1)
+                }
+            }
+        }
 
         // TODO: this can be optimized a lot, e.g. with a ring-like queue
         if (chipCountQueue == null) {
